f6152f14dafb682b817fe2b8adf38666393cad22
Simeon <simeoncoding@gmail.com>
2019-03-19 18:11:04 +0000

    add pages

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Layout from "../components/Layout.js";
+import Link from "next/link";
+
+const PostLink = props => (
+  <li>
+    {/* "as" is the URL which is shown on the browser. The URL the app sees is "href" prop. */}
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+
+export default () => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink id="hello-nextjs" title="Hello Next.js" />
+      <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
+      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+    </ul>
+  </Layout>
+);
