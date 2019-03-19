0970dbc528a4d40c6eb4d7a9ebce82a524f40229
Simeon <simeoncoding@gmail.com>
2019-03-19 18:20:18 +0000

    remove unused link in the header

--- components/Header.js
+++ components/Header.js
@@ -12,9 +12,6 @@ const Header = () => (
     <Link href="/about">
       <a style={linkStyle}>About</a>
     </Link>
-    <Link href="/sample">
-      <a style={linkStyle}>Sample</a>
-    </Link>
   </div>
 );
 
