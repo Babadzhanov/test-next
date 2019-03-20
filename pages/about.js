0ffd02e35a3ed8cf751a0dd382e0ab28e73872b4
Simeon <simeoncoding@gmail.com>
2019-03-20 23:26:48 +0000

    refactor test code in about page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,12 @@
-import Link from 'next/link'
+import Link from "next/link";
 export default () => (
   <div>
-    <div>About us</div>
-    <div>Back to <Link href='/' as={ process.env.BACKEND_URL + '/'}><a>Home</a></Link></div>
+    <h1>About page</h1>
+    <div>
+      Back to{" "}
+      <Link href="/" as={process.env.BACKEND_URL + "/"}>
+        <a>Home</a>
+      </Link>
+    </div>
   </div>
-)
+);
