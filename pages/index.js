54daede6bf9e167271d747c85a8cfa0a3422609c
Simeon <simeoncoding@gmail.com>
2019-04-05 18:36:42 +0100

    sample

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default () => (
   <div>
-    <h1>Index page</h1>
+    <h1>Index page.</h1>
     <div>
       Hello World.
       <Link href="/about" as={process.env.BACKEND_URL + "/about"}>
