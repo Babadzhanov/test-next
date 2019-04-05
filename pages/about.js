5fbfa86aade9dc7f861658e6ed7ce2f2cc391a5d
Simeon <simeoncoding@gmail.com>
2019-04-05 17:20:58 +0100

    simple test

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default () => (
   <div>
-    <h1>About page</h1>
+    <h1>About page.</h1>
     <div>
       Back to{" "}
       <Link href="/" as={process.env.BACKEND_URL + "/"}>
