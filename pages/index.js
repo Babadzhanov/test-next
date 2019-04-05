de58830ac1fc28a2c6720ba3c63e4cb162597f49
Simeon <simeoncoding@gmail.com>
2019-04-05 19:11:26 +0100

    finaltest

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ export default () => (
   <div>
     <h1>Index page</h1>
     <div>
-      Hello World.
+      Hello World!
       <Link href="/about" as={process.env.BACKEND_URL + "/about"}>
         <a>About</a>
       </Link>
