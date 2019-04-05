9d07381caa9e1d96636ca9dae1101ae91461b9aa
Simeon <simeoncoding@gmail.com>
2019-04-05 16:27:00 +0100

    edit next.config file with project name

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ module.exports = {
     }
   },
   //assetPrefix: '',
-  assetPrefix: !debug ? '/Next-gh-page-example/' : '',
+  assetPrefix: !debug ? '/test-next/' : '',
   webpack: (config, { dev }) => {
     // Perform customizations to webpack config
     // console.log('webpack');
