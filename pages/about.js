4b0dce454c35d5d65108e0b06a64832fa5f31525
Simeon <simeoncoding@gmail.com>
2019-04-08 11:25:03 +0100

    test styling

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,15 @@
 import Link from "next/link";
+import styled from "styled-components";
 
 import Layout from "../components/layout";
 
+const H1 = styled.h1`
+  color: red;
+`;
+
 export default () => (
   <Layout>
-    <h1>About page.</h1>
+    <H1>About page.</H1>
     <div>
       Back to{" "}
       <Link href="/" as={process.env.BACKEND_URL + "/"}>
