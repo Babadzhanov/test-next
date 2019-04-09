b52c5909c6b7e322dfab03489bbcb381ea64c456
Simeon <simeoncoding@gmail.com>
2019-04-09 21:56:56 +0100

    reduce code on index page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,7 @@
-import Link from "next/link";
-
 import Layout from "../components/layout";
 
 export default () => (
   <Layout>
-    <h1>Index page.</h1>
-    <div>
-      Hello World!
-      <Link href="/about" as={process.env.BACKEND_URL + "/about"}>
-        <a>About page</a>
-      </Link>
-    </div>
+    <h1>Index page</h1>
   </Layout>
 );
