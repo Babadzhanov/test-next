14d714ab1b683debecb591a35ccca6203a1ba905
Simeon <simeoncoding@gmail.com>
2019-04-10 19:05:51 +0100

    add global style file for stlyed components

---
+++ theme/index.js
@@ -0,0 +1,16 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+    body {
+      padding: 0;
+      margin: 0;
+      overflow-x: hidden;
+      background-color: #f1f1f1;
+    }
+
+    * {
+        font-family: 'Barlow', sans-serif;
+    }
+
+  `;
